25c43358c437000578b588856058f9338cf1a718
Defrangod <49184809+Defrangod@users.noreply.github.com>
2019-11-22 17:15:20 +0300

    кусок обновы

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Common;
+using Practice;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AuthorizationAdminTest()
+        {
+            //Arrange
+            string login = "12";
+            string password = "1";
+            string expectdRole = "Администратор";
+            string actualRole = "Администратор";
+            //Act
+            DBConnection.Connect();
+            DBConnection.Autorization(login, password);
+            actualRole = DBConnection.Role;
+            //Assert
+            Assert.AreEqual(expectdRole, actualRole);
+        }
+    }
+}
